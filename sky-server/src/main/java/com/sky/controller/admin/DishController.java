17b49fbd31717a97760b897d72476d8074fb161f
Gbb <3263114159@qq.com>
2023-08-18 18:05:40 +0800

    缓存菜品数据功能代码开发

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -11,9 +11,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * 菜品管理
@@ -26,11 +28,19 @@ public class DishController {
 
     @Autowired
     public DishService dishService;
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+
     @PostMapping
     @ApiOperation("新增菜品")
     public Result save(@RequestBody DishDTO dishDTO){
         log.info("新增菜品:{}",dishDTO);
         dishService.saveWithFlavor(dishDTO);
+
+        //清理缓存数据
+        String key = "dish_"+dishDTO.getCategoryId();
+        clearCache(key);
         return Result.success();
     }
 
@@ -47,6 +57,10 @@ public class DishController {
     public Result delete(@RequestParam List<Long> ids){
         log.info("菜品批量删除：{}",ids);
         dishService.deleteBatch(ids);
+
+        //将所有清理缓存数据
+        clearCache("dish_*");
+
         return Result.success();
     }
 
@@ -63,6 +77,10 @@ public class DishController {
     public Result update(@RequestBody DishDTO dishDTO){
         log.info("修改菜品信息：{}",dishDTO);
         dishService.updateWithFlavor(dishDTO);
+
+        //将所有清理缓存数据
+        clearCache("dish_*");
+
         return Result.success();
     }
 
@@ -72,5 +90,26 @@ public class DishController {
         log.info("根据分类id查询菜品");
         List<Dish> list = dishService.list(categoryId);
         return Result.success(list);
+    }
+
+    @PostMapping("/status/{status}")
+    @ApiOperation("菜品起售停售")
+    public Result<String> startOrStop(@PathVariable Integer status,Long id){
+        dishService.startOrStop(status,id);
+
+        //将所有清理缓存数据
+        clearCache("dish_*");
+
+        return Result.success();
+    }
+
+    /**
+     * 统一清除缓存数据
+     * @param pattern
+     */
+    private void clearCache(String pattern) {
+        //将所有清理缓存数据
+        Set keys = redisTemplate.keys(pattern);
+        redisTemplate.delete(keys);
     }
 }
--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -9,6 +9,7 @@ import com.sky.vo.DishVO;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -70,4 +71,5 @@ public interface DishMapper {
 
     @Select("select * from setmeal_dish where setmeal_id = #{id}")
     List<Dish> getBySetmealId(Long id);
+
 }
